bb6741abaa9f7c38d55acbbf0fdf135b3173b4d1
Chandra1st <chandragunawanwijaya_2327250023@mhs.mdp.ac.id>
2024-10-28 11:30:34 +0700

    postman

--- buku_perpustakaan/routes/api.php
+++
@@ -1,5 +0,0 @@
-<?php
-use App\Http\Controllers\Api\BukuController;
-
-Route::get('/bukus', [BukuController::class, 'index']);
-Route::post('/bukus', [BukuController::class, 'store']);
---
+++ perpustakaan_buku/README.md
@@ -0,0 +1,3 @@
+![alt text](images/image.png)
+![alt text](image-1.png)
+![alt text](image-2.png)
